b77a1d01b10d77f8b08c8b539bd01960ee86e5ab
Ignacio Soler <ignaciojsoler@gmail.com>
2026-01-02 23:11:48 -0300

    Resolver chains

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Amenity, Listing } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
@@ -10,5 +10,10 @@ export class ListingAPI extends RESTDataSource {
 
   getListing(listingId: string) {
     return this.get<Listing>(`listings/${listingId}`);
+  }
+
+  getAmenities(listingId: string) {
+    console.log("Making a follow-up call for amenities with ", listingId);
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
   }
 } 
